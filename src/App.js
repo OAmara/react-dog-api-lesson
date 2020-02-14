6546c788c175b6385ad86767cd0c0286a7e61fa3
Omar Amara <O_amara@hotmail.com>
2020-02-14 11:28:47 -0600

    Created LoginRegisterForm, made app.js smart component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import DogContainer from './DogContainer'
+import LoginRegisterForm from './LoginRegisterForm'
 
-function App() {
-  // console.log(process.env);
-  return (
-    <div className="App">
-    <h1>Dogs</h1>
-    <DogContainer />
-    </div>
-  );
+class App extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			loggedIn: false,
+			loggedInUserEmail: null, // might be helpful to track
+		}
+	}
+	render() {
+  		// console.log(process.env);
+		return (
+		    <div className="App">
+			    <h1>Dogs</h1>
+			    { this.state.loggedIn ? <DogContainer /> : <LoginRegisterForm />}
+		    </div>
+		);
+	}
 }
 
 export default App;
